bb44dcf1eabf1a246f22c3d72f3c453010255c93
Brian Zheng <brianzheng205@gmail.com>
2024-10-09 18:05:25 -0400

    Create smileys on scale puzzle completion

--- Assets/Scripts/AnalogScale.cs
+++ Assets/Scripts/AnalogScale.cs
@@ -6,6 +6,8 @@ public class AnalogScale : MonoBehaviour
     public ScaleObject[] allScaleObjectsOnScale;
     public GameObject leftScale;
     public GameObject rightScale;
+    public GameObject smileyA;
+    public GameObject smileyB;
 
     private int leftWeight = 0;
     private int rightWeight = 0;
@@ -18,8 +20,10 @@ public class AnalogScale : MonoBehaviour
 
     void Update()
     {
+        // calculate left weight
         leftWeight = digitalScale.GetWeight();
 
+        // calculate right weight
         int newRightWeight = 0;
         foreach (ScaleObject scaleObject in allScaleObjectsOnScale)
         {
@@ -31,6 +35,7 @@ public class AnalogScale : MonoBehaviour
 
         rightWeight = newRightWeight;
 
+        // calculate heights of scales and objects on right scale
         if (leftWeight > rightWeight)
         {
             leftHeight = -(leftWeight - rightWeight) / SCALE + OFFSET;
@@ -42,15 +47,29 @@ public class AnalogScale : MonoBehaviour
             rightHeight = -(rightWeight - leftWeight) / SCALE + OFFSET;
         }
 
+        // set left and right scale heights
         leftScale.transform.localPosition = new Vector3(leftScale.transform.localPosition.x, leftHeight, leftScale.transform.localPosition.z);
         rightScale.transform.localPosition = new Vector3(rightScale.transform.localPosition.x, rightHeight, rightScale.transform.localPosition.z);
 
+        // set object heights on right scale
         foreach (ScaleObject scaleObject in allScaleObjectsOnScale)
         {
             if (scaleObject.gameObject.activeSelf)
             {
                 scaleObject.transform.localPosition = new Vector3(scaleObject.transform.localPosition.x, rightHeight + SCALE_OBJECTS_OFFSET, scaleObject.transform.localPosition.z);
             }
+        }
+
+        // active or deactivate smiley
+        if (leftWeight == rightWeight && !smileyA.activeSelf)
+        {
+            smileyA.SetActive(true);
+            smileyB.SetActive(true);
+        }
+        else if (leftWeight != rightWeight && smileyA.activeSelf)
+        {
+            smileyA.SetActive(false);
+            smileyB.SetActive(false);
         }
     }
 }
