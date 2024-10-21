783b7fdac1bd18322a634083fe45e093e9b74496
Ayden Johnson <67842316+ADJohnson314@users.noreply.github.com>
2024-10-21 17:05:26 -0400

    Refine Wire Puzzle

--- Assets/Scripts/Wire Puzzle.cs
+++ Assets/Scripts/Wire Puzzle.cs
@@ -5,30 +5,53 @@ using System;
 
 public class WirePuzzle : MonoBehaviour
 {
-    public GameObject tilePrefab;
-    /*
-    public int width = 4
-    public int height = 4
-    */
+    private int puzzleSize = 4;  // Assumes the puzzle is a square grid
 
-    private GameObject[] tiles = new GameObject[16];
+    public GameObject tilePrefab;
+    public GameObject wirePrefabA;
+    public GameObject wirePrefabB;
+    public GameObject wirePrefabC;
+    public float wireThickness = 0.05f;
+    private GameObject[] tiles;
     private bool solved = false;
     private WireTile tileScript;
+    private int[] wirePath;
 
     public GameObject leftView;
     public GameObject rightView;
     public GameObject testIndicatorL;
     public GameObject testIndicatorR;
+
+    private int RIGHT;
+    private int UP;
+    private int LEFT;
+    private int DOWN;
+    private int nullInt;
+
+    private System.Random rnd = new System.Random();
+
     public GameObject numberToActivate1;
     public GameObject numberToActivate2;
     public AudioSource finishAudio;
 
     // Start is called before the first frame update
     void Start() {
-        System.Random rnd = new System.Random();
-        int randAngle;
-        for (int r = 0; r < 4; r++) {
-            for (int c = 0; c < 4; c++) {
+        tiles = new GameObject[puzzleSize*puzzleSize];
+        wirePath = new int[puzzleSize*puzzleSize*2 + 1];
+
+        RIGHT = 1;
+        UP = -1*puzzleSize;
+        LEFT = -1;
+        DOWN = puzzleSize;
+        nullInt = -1*puzzleSize - 1;
+
+        Vector3 relTileShift = new Vector3(0, 2, 0);
+
+        // Instantiate tiles and set rotations
+        int randAngle = 0;
+        for (int r = 0; r < puzzleSize; r++) {
+            for (int c = 0; c < puzzleSize; c++) {
+
                 if ((r+c) % 3 == 0) {
                     randAngle = rnd.Next(2)*180 + 90;
                 }
@@ -37,78 +60,86 @@ public class WirePuzzle : MonoBehaviour
                 }
 
                 if ((r+c) % 2 == 0) {
-                    tiles[4*r+c] = Instantiate(tilePrefab, transform.position + new Vector3(c-1.5f-4.5f, 1.5f-r, 0), transform.rotation * Quaternion.Euler(0, 0, randAngle), leftView.transform);
+                    tiles[puzzleSize*r+c] = Instantiate(tilePrefab, leftView.transform.position + relTileShift + new Vector3(c-1.5f, 1.5f-r, 0), leftView.transform.rotation * Quaternion.Euler(0, 0, randAngle), leftView.transform);
                 }
                 else {
-                    tiles[4*r+c] = Instantiate(tilePrefab, transform.position + new Vector3(c-1.5f+4.5f, 1.5f-r, 0), transform.rotation * Quaternion.Euler(0, 0, randAngle), rightView.transform);
+                    tiles[puzzleSize*r+c] = Instantiate(tilePrefab, rightView.transform.position + relTileShift + new Vector3(c-1.5f, 1.5f-r, 0), rightView.transform.rotation * Quaternion.Euler(0, 0, randAngle), rightView.transform);
                 }
+                tileScript = tiles[puzzleSize*r+c].GetComponent<WireTile>();
+                tileScript.wirePrefab = wirePrefabA;
+                if (wirePrefabB != null) {
+                    tileScript.wirePrefab1 = wirePrefabB;
+                }
+                if (wirePrefabC != null) {
+                    tileScript.wirePrefab2 = wirePrefabC;
+                }
+                tileScript.wireThickness = wireThickness;
             }
         }
 
-        // Hardcoding a wire path for now...
-        tileScript = tiles[0].GetComponent<WireTile>();
-        tileScript.rightWire = true;
-        tileScript.downWire = true;
 
-        tileScript = tiles[1].GetComponent<WireTile>();
-        tileScript.leftWire = true;
-        tileScript.downWire = true;
+        // Debug.Log("Test 1: " + isAdjacent(new int[] {1, 2, 3, 4, 5, 6, 2, 4}, 2, 5));
+        // Debug.Log("Test 2: " + isAdjacent(new int[] {1, 2, 3, 4, 5, 6, 2, 4}, 2, 3));
+        // Debug.Log("Test 3: " + isAdjacent(new int[] {1, 2, 3, 4, 5, 6, 2, 4}, 2, 6));
 
-        tileScript = tiles[2].GetComponent<WireTile>();
-        tileScript.rightWire = true;
-        tileScript.upWire = true;
+        // Generate random path(s) and set wires for each wire prefab
+        GameObject[] prefabList = new GameObject[] {wirePrefabA, wirePrefabB, wirePrefabC};
+        for (int p = 0; p < 3; p++) {
+            if (prefabList[p] != null) {
 
-        tileScript = tiles[3].GetComponent<WireTile>();
-        tileScript.leftWire = true;
-        tileScript.downWire = true;
+                bool validPath = writeRandomPath(wirePath);
+                while (!validPath) {
+                    validPath = writeRandomPath(wirePath);
+                }
 
-        tileScript = tiles[4].GetComponent<WireTile>();
-        tileScript.upWire = true;
-        tileScript.downWire = true;
+                /*
+                string pathText = wirePath[0].ToString();
+                for (int l = 1; l < wirePath.Length; l++) {
+                    pathText = pathText + ", " + wirePath[l].ToString();
+                }
 
-        tileScript = tiles[5].GetComponent<WireTile>();
-        tileScript.upWire = true;
-        tileScript.downWire = true;
-        
-        tileScript = tiles[6].GetComponent<WireTile>();
-        tileScript.rightWire = true;
-        tileScript.downWire = true;
+                Debug.Log(pathText);
+                */
 
-        tileScript = tiles[7].GetComponent<WireTile>();
-        tileScript.leftWire = true;
-        tileScript.upWire = true;
+                float[] wireLengths = new float[] {0.5f + wireThickness/2 - 2*wireThickness, 0.5f + wireThickness/2, 0.5f + wireThickness/2 + 2*wireThickness};
+                float[] relWireShifts = new float[] {0.5f - (wireLengths[0])/2, 0.5f - (wireLengths[1])/2, 0.5f - (wireLengths[2])/2};
+                int[] helper = new int[] {0, 1, -1};
+                GameObject wire;
 
-        tileScript = tiles[8].GetComponent<WireTile>();
-        tileScript.upWire = true;
-        tileScript.downWire = true;
+                int startCol = wirePath[0] % puzzleSize;
+                Vector3 startPos = relTileShift + new Vector3(startCol-1.5f, 1.5f-4, 0) + new Vector3(-helper[p]*2*wireThickness, relWireShifts[1-helper[p]]*this.transform.localScale.y, -1);
+                if ((3+startCol) % 2 == 0) {
+                    wire = Instantiate(prefabList[p], leftView.transform.position + startPos, leftView.transform.rotation * Quaternion.Euler(0, 0, 90), leftView.transform);
+                    wire.transform.localScale = new Vector3(wireLengths[1-helper[p]], wireThickness, 1);
+                }
+                else {
+                    wire = Instantiate(prefabList[p], rightView.transform.position + startPos, rightView.transform.rotation * Quaternion.Euler(0, 0, 90), rightView.transform);
+                    wire.transform.localScale = new Vector3(wireLengths[1-helper[p]], wireThickness, 1);
+                }
 
-        tileScript = tiles[9].GetComponent<WireTile>();
-        tileScript.upWire = true;
-        tileScript.downWire = true;
+                // endCol is the array index of the last tile location in the path
+                int endCol = Array.IndexOf(wirePath, -5);
+                if (endCol == -1) {
+                    endCol = wirePath.Length - 1;
+                }
+                else {
+                    endCol = endCol - 1;
+                }
+                endCol = wirePath[endCol] + puzzleSize; // endCol is now the column of the last location (a negative number between -puzzleSize and -1)
+                Vector3 endPos = relTileShift + new Vector3(endCol-1.5f, 1.5f+1, 0) + new Vector3(-helper[p]*2*wireThickness, -relWireShifts[helper[p]+1]*this.transform.localScale.y, -1);
+                if (endCol % 2 == 0) {
+                    wire = Instantiate(prefabList[p], leftView.transform.position + endPos, leftView.transform.rotation * Quaternion.Euler(0, 0, 270), leftView.transform);
+                    wire.transform.localScale = new Vector3(wireLengths[helper[p]+1], wireThickness, 1);
+                }
+                else {
+                    wire = Instantiate(prefabList[p], rightView.transform.position + endPos, rightView.transform.rotation * Quaternion.Euler(0, 0, 270), rightView.transform);
+                    wire.transform.localScale = new Vector3(wireLengths[helper[p]+1], wireThickness, 1);
+                }
 
-        tileScript = tiles[10].GetComponent<WireTile>();
-        tileScript.rightWire = true;
-        tileScript.upWire = true;
 
-        tileScript = tiles[11].GetComponent<WireTile>();
-        tileScript.leftWire = true;
-        tileScript.downWire = true;
-
-        tileScript = tiles[12].GetComponent<WireTile>();
-        tileScript.upWire = true;
-        tileScript.downWire = true;
-
-        tileScript = tiles[13].GetComponent<WireTile>();
-        tileScript.upWire = true;
-        tileScript.rightWire = true;
-        
-        tileScript = tiles[14].GetComponent<WireTile>();
-        tileScript.rightWire = true;
-        tileScript.leftWire = true;
-
-        tileScript = tiles[15].GetComponent<WireTile>();
-        tileScript.leftWire = true;
-        tileScript.upWire = true;
+                setWires(wirePath, p);
+            }
+        }
     }
 
     // Update is called once per frame
@@ -130,5 +161,119 @@ public class WirePuzzle : MonoBehaviour
             finishAudio.Play();
         }
         solved = currSolved;
+    }
+
+    int locPlusDir(int location, int direction)
+    {
+        // This function takes a location and calculates the location one step in the given direction, or returns the null integer if this
+        // location isn't valid
+
+        if (location % puzzleSize == 0 && direction == LEFT) {
+            // If the location is on the left edge of the grid and the direction is left, return nullInt
+            return nullInt;
+        }
+
+        else if ((location+1) % puzzleSize == 0 && direction == RIGHT) {
+            // If the location is on the right edge of the grid and the direction is right, return nullInt
+            return nullInt;
+        }
+
+        else if ((puzzleSize*puzzleSize - puzzleSize <= location) && (location < puzzleSize*puzzleSize) && (direction == DOWN)) {
+            // If the location is on the bottom edge of the grid and the direction is down, return nullInt
+            return nullInt;
+        }
+
+        else {
+            return location + direction;
+        }
+    }
+
+    bool isAdjacent(int[] path, int loc1, int loc2)
+    {
+        // This returns True if the two given locations appear adjacent in the given array, and False otherwise
+        for (int i = 0; i < path.Length-1; i++) {
+            if ((path[i] == loc1 && path[i+1] == loc2) || (path[i] == loc2 && path[i+1] == loc1)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    bool writeRandomPath(int[] path) {
+        // This generates a random path from the bottom of the puzzle grid to the top.
+        // It writes the path inside of the given path array, starting with filling the array with nullInts.
+        // It returns True if the path successfully reaches the top of the grid, and False otherwise.
+
+        for (int i = 0; i < path.Length; i++) {
+            path[i] = nullInt;
+        }
+
+        path[0] = rnd.Next(puzzleSize*puzzleSize, puzzleSize*(puzzleSize+1));
+        // Debug.Log("Selected first location: " + path[0]);
+        path[1] = locPlusDir(path[0], UP);
+        // Debug.Log("Selected next location: " + path[1]);
+
+        int[] newLocs = new int[4];
+        bool[] possibleDirs = new bool[4];
+        for (int j = 2; j < path.Length; j++) {
+            // To append a new location to the end of the path, find all of the possible directions to move from the last location, then choose
+            // one at random and enter the resulting location into the array.
+
+            newLocs[0] = locPlusDir(path[j-1], RIGHT);
+            newLocs[1] = locPlusDir(path[j-1], UP);
+            newLocs[2] = locPlusDir(path[j-1], LEFT);
+            newLocs[3] = locPlusDir(path[j-1], DOWN);
+            
+            for (int k = 0; k < 4; k++) {
+                possibleDirs[k] = newLocs[k] != -1 && !(isAdjacent(path, newLocs[k], path[j-1]));
+            }
+
+            if (Array.TrueForAll(possibleDirs, d => !d)) {
+                return false;
+            }
+
+            int randK = (int) (rnd.Next(7)/2 + 0.5f);
+            // Debug.Log("RNG: " + randK);
+            while (!possibleDirs[randK]) {
+                randK = (int) (rnd.Next(7)/2 + 0.5f);
+                // Debug.Log("RNG: " + randK);
+            }
+
+            path[j] = newLocs[randK];
+            if (path[j] < 0 && path[j] >= -1*puzzleSize) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    void setWires(int[] path, int wireIndex)
+    {
+        // This sets the wire bool variables of each tile to match the generated path.
+        for (int i = 1; i < path.Length; i++) {
+            if (path[i] < 0) {
+                break;
+            }
+
+            tileScript = tiles[path[i]].GetComponent<WireTile>();
+            int deltaPri = path[i-1] - path[i];  // delta from current location to last location
+            int deltaPost = path[i+1] - path[i]; // delta from current location to next location
+            Debug.Assert(deltaPri != deltaPost);
+
+            if (deltaPri == RIGHT || deltaPost == RIGHT) {
+                tileScript.rightWires[wireIndex] = true;
+            }
+            if (deltaPri == UP || deltaPost == UP) {
+                tileScript.upWires[wireIndex] = true;
+            }
+            if (deltaPri == LEFT || deltaPost == LEFT) {
+                tileScript.leftWires[wireIndex] = true;
+            }
+            if (deltaPri == DOWN || deltaPost == DOWN) {
+                tileScript.downWires[wireIndex] = true;
+            }
+        }
     }
 }
--- Assets/Scripts/Wire Tile.cs
+++ Assets/Scripts/Wire Tile.cs
@@ -6,54 +6,67 @@ using System;
 public class WireTile : MonoBehaviour
 {
     // These variables control what wires are created at the start of runtime
-    public bool rightWire;
-    public bool upWire;
-    public bool leftWire;
-    public bool downWire;
-    public GameObject wirePrefab;
+    public bool[] rightWires = new bool[3];
+    public bool[] upWires = new bool[3];
+    public bool[] leftWires = new bool[3];
+    public bool[] downWires = new bool[3];
 
-    private bool[] wireArray = new bool[4];
-    private bool[] wireArrayOrient = new bool[4];
+    public GameObject wirePrefab;
+    public GameObject wirePrefab1;
+    public GameObject wirePrefab2;
+    private GameObject[] prefabList;
+
+    public float wireThickness = 0.05f;
+
+    private bool oneWireType;
+    private bool Line;
+    private bool XO;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        float relWireShift = 0.5f - (wirePrefab.transform.localScale.x)/2;
-        wireArray[0] = rightWire;
-        wireArray[1] = upWire;
-        wireArray[2] = leftWire;
-        wireArray[3] = downWire;
+        prefabList = new GameObject[] {wirePrefab, wirePrefab1, wirePrefab2};
 
-        wireArrayOrient[0] = rightWire;
-        wireArrayOrient[1] = upWire;
-        wireArrayOrient[2] = leftWire;
-        wireArrayOrient[3] = downWire;
+        // This is true if there's only one wire type on the tile
+        oneWireType = !rightWires[1] && !rightWires[2] && !upWires[1] && !upWires[2] && !leftWires[1] && !leftWires[2] && !downWires[1] && !downWires[2];
 
-        //Debug.Log("Tile rotation: " + transform.rotation.z);
+        // These variables are set to true if there are two wires that make a straight line vertically or horizontally, and only one type of wire
+        Line = (leftWires[0] == rightWires[0]) && oneWireType;
+        XO = ((rightWires[0] == upWires[0]) && (upWires[0] == leftWires[0]) && (leftWires[0] == downWires[0])) && oneWireType;
 
-        //Debug.Log("Starting orient: " + wireArrayOrient[0] + ", " + wireArrayOrient[1] + ", " + wireArrayOrient[2] + ", " + wireArrayOrient[3]);
+        float[] wireLengths = new float[] {0.5f + wireThickness/2 - 2*wireThickness, 0.5f + wireThickness/2, 0.5f + wireThickness/2 + 2*wireThickness};
+        float[] relWireShifts = new float[] {0.5f - (wireLengths[0])/2, 0.5f - (wireLengths[1])/2, 0.5f - (wireLengths[2])/2};
+        int[] helper = new int[] {0, 1, -1};
+        GameObject wire;
 
-        /*
-        for (int i = 0; i < 4; i++) {
-            if (wireArray[i]) {
-                Instantiate(wirePrefab, transform.position + new Vector3(relWireShift*this.transform.localScale.x, 0, 0), transform.rotation, this.transform);
+        // This chain of blocks creates the wires for each tile.
+        for (int r = 0; r < 3; r++) {
+            if (rightWires[r]) {
+                wire = Instantiate(prefabList[r], transform.position + (Quaternion.AngleAxis(transform.rotation.eulerAngles.z, Vector3.forward) * new Vector3(relWireShifts[helper[r]+1]*this.transform.localScale.x, helper[r]*2*wireThickness, -1)), transform.rotation, this.transform);
+                wire.transform.localScale = new Vector3(wireLengths[helper[r]+1], wireThickness, 1);
             }
         }
-        */
+        for (int u = 0; u < 3; u++) {
+            if (upWires[u]) {
+                wire = Instantiate(prefabList[u], transform.position + (Quaternion.AngleAxis(transform.rotation.eulerAngles.z, Vector3.forward) * new Vector3(-helper[u]*2*wireThickness, relWireShifts[1-helper[u]]*this.transform.localScale.y, -1)), transform.rotation * Quaternion.Euler(0, 0, 90), this.transform);
+                wire.transform.localScale = new Vector3(wireLengths[1-helper[u]], wireThickness, 1);
+            }
+        }
+        for (int l = 0; l < 3; l++) {
+            if (leftWires[l]) {
+                wire = Instantiate(prefabList[l], transform.position + (Quaternion.AngleAxis(transform.rotation.eulerAngles.z, Vector3.forward) * new Vector3(-relWireShifts[1-helper[l]]*this.transform.localScale.x, helper[l]*2*wireThickness, -1)), transform.rotation * Quaternion.Euler(0, 0, 180), this.transform);
+                wire.transform.localScale = new Vector3(wireLengths[1-helper[l]], wireThickness, 1);
+            }
+        }
+        for (int d = 0; d < 3; d++) {    
+            if (downWires[d]) {
+                wire = Instantiate(prefabList[d], transform.position + (Quaternion.AngleAxis(transform.rotation.eulerAngles.z, Vector3.forward) * new Vector3(-helper[d]*2*wireThickness, -relWireShifts[helper[d]+1]*this.transform.localScale.y, -1)), transform.rotation * Quaternion.Euler(0, 0, 270), this.transform);
+                wire.transform.localScale = new Vector3(wireLengths[helper[d]+1], wireThickness, 1);
+            }
+        }
 
-        if (rightWire) {
-            Instantiate(wirePrefab, transform.position + (Quaternion.AngleAxis(transform.rotation.eulerAngles.z, Vector3.forward) * new Vector3(relWireShift*this.transform.localScale.x, 0, -1)), transform.rotation, this.transform);
-        }
-        if (upWire) {
-            Instantiate(wirePrefab, transform.position + (Quaternion.AngleAxis(transform.rotation.eulerAngles.z, Vector3.forward) * new Vector3(0, relWireShift*this.transform.localScale.y, -1)), transform.rotation * Quaternion.Euler(0, 0, 90), this.transform);
-        }
-        if (leftWire) {
-            Instantiate(wirePrefab, transform.position + (Quaternion.AngleAxis(transform.rotation.eulerAngles.z, Vector3.forward) * new Vector3(-relWireShift*this.transform.localScale.x, 0, -1)), transform.rotation * Quaternion.Euler(0, 0, 180), this.transform);
-        }
-        if (downWire) {
-            Instantiate(wirePrefab, transform.position + (Quaternion.AngleAxis(transform.rotation.eulerAngles.z, Vector3.forward) * new Vector3(0, -relWireShift*this.transform.localScale.y, -1)), transform.rotation * Quaternion.Euler(0, 0, 270), this.transform);
-        }
+        updateTileRotation();
     }
 
     // Update is called once per frame
@@ -66,23 +79,18 @@ public class WireTile : MonoBehaviour
     void OnMouseDown()
     {
         transform.Rotate(0, 0, 90);
-        int newAngle = (int)transform.rotation.eulerAngles.z;
-        int arrInd;
-        for (int i = 0; i < 4; i++) {
-            arrInd = (i-newAngle/90);
-            while (arrInd < 0) {
-                arrInd += 4;
-            }
-            wireArrayOrient[i] = wireArray[arrInd % 4];
-        }
-        
-        Debug.Log("New orient:" + wireArrayOrient[0] + ", " + wireArrayOrient[1] + ", " + wireArrayOrient[2] + ", " + wireArrayOrient[3]);
-        Debug.Log("Zero orient:" + wireArray[0] + ", " + wireArray[1] + ", " + wireArray[2] + ", " + wireArray[3]);
+        updateTileRotation();
+    }
 
-        if (wireArrayOrient[0] == wireArray[0] && wireArrayOrient[1] == wireArray[1] && wireArrayOrient[2] == wireArray[2] && wireArrayOrient[3] == wireArray[3]) {
+    void updateTileRotation()
+    {
+        // If multiple orientations of a tile can solve the puzzle, this function makes sure that all of them register as the 0 rotation.
+        if (Line && transform.rotation.eulerAngles.z % 180 == 0) {
             transform.rotation = Quaternion.Euler(0, 0, 0);
-            Debug.Log("Bup!");
+        }
 
+        if (XO && transform.rotation.eulerAngles.z % 90 == 0) {
+            transform.rotation = Quaternion.Euler(0, 0, 0);
         }
     }
 }
